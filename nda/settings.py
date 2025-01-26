ed4dc839ecd897df48216f7a22616266e7ab6a77
Am-Am-Am <amur_almukhametov@mail.ru>
2025-01-26 22:47:00 +0500

    debug false

--- nda/settings.py
+++ nda/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=j#*b5xl1dgzvmdrf9zc+qae3z7^uqie)rao-_*okz+=tboh2-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
