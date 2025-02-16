292a3582ebe9080a4cb0ab5b8e3663481ba7ef7c
Am-Am-Am <amur_almukhametov@mail.ru>
2025-02-16 15:52:21 +0500

    Исправленный поиск на мобильной версии

--- nda/settings.py
+++ nda/settings.py
@@ -125,7 +125,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        # "BACKEND": 'django.core.cache.backends.dummy.DummyCache',
+        "BACKEND": 'django.core.cache.backends.dummy.DummyCache',
         "LOCATION": "redis://127.0.0.1:6379",
     }
 }
